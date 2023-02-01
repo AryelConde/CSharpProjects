9cd4b2e9f11c29e1844f9d1a9dc117043403077f
AryelConde <123709225+AryelConde@users.noreply.github.com>
2023-02-01 11:36:35 -0600

    Add files via upload

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Globalization;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the first number: ");
+            String input1 = Console.ReadLine();
+            float num1 = float.Parse(input1);
+
+            Console.Write("Input the second number: ");
+            String input2 = Console.ReadLine();
+            float num2 = float.Parse(input2);
+
+            Console.Write("Input the third number: ");
+            String input3 = Console.ReadLine();
+            float num3 = float.Parse(input3);
+
+
+
+            Console.WriteLine("\nThe results are: ");
+            float addTotal = num1 + num2 + num3;
+            float multTotal = num1 * num2 * num3;
+            Console.WriteLine(num1 + " + " + num2 + " + " + num3 + " = " + addTotal);
+            Console.WriteLine(num1 + " x " + num2 + " x " + num3 + " = " + multTotal);
+        }
+    }
+}
---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Please enter the following data: ");
+            Console.Write("Name: ");
+            String input = Console.ReadLine();
+            Console.Write("Student number: ");
+            string input2 = Console.ReadLine();
+            Console.Write("Email: ");
+            String input3 = Console.ReadLine();
+
+            Console.WriteLine("\nYour contact data is: ");
+            Console.WriteLine("Your name is: "+input);
+            Console.WriteLine("Your student number is: " + input2);
+            Console.WriteLine("Your email is: " + input3);
+        }
+    }
+}
